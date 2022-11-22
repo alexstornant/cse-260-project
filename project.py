35b77e26263604971f4793742870b3137bf9495e
alexstornant <stornant.alex@gmail.com>
2022-11-22 16:52:06 -0500

    added thing to display board if sat

--- project.py
+++ project.py
@@ -1,4 +1,6 @@
 from z3 import *
+import numpy as np
+import matplotlib.pyplot as plt
 
 n = int(input("Please input a number: "))
 
@@ -15,7 +17,6 @@ Q_4 = And([Or(Not(Q[i][j] == 1), Not(Q[i-k][k+j] == 1)) for i in range(1, n) for
 
 Q_5 = And([Or(Not(Q[i][j] == 1), Not(Q[i+k][k+j] == 1)) for i in range(n-1) for j in range(n-1) for k in range(min(n-i, n-j))])
 
-
 s = Solver()
 s.add(Q_1, Q_2, Q_3)
 s.add(Q_4)
@@ -23,6 +24,17 @@ s.add(Q_5)
 
 try:
    s.check()
-   print(s.model())
+
+   m = s.model()
+   board = np.zeros((n,n))
+
+   for i in range(n):
+      for j in range(n):
+         if m[Q[i][j]] == 1:
+            board[i][j] = 10
+
+   plt.matshow(board)
+   plt.show()
+
 except:
    print(s.check())
