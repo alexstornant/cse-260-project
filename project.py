a5a9939790349811e1dfebdfe63ec72fbe17c1ff
alexstornant <stornant.alex@gmail.com>
2022-11-22 14:01:38 -0500

    refactoring solution to use the project guidelines, got Q1 - Q3 done

--- project.py
+++ project.py
@@ -1,22 +1,23 @@
 from z3 import *
 
-n = input("Please input a number: ")
-n = int(n)
+n = int(input("Please input a number: "))
 
-# We know each queen must be in a different row.
-# So, we represent each queen by a single integer: the column position
-Q = [ Int(f"Q_{row + 1}") for row in range(n) ]
+# Queen is in position Q[i][j], if Q[i][j] == 1, else 0
+Q = [ [Int(f"Q_{i + 1}_{j + 1}") for j in range(n) ] for i in range(n) ]
 
-# Each queen is in a column {1, ... n }
-val_c = [ And(1 <= Q[row], Q[row] <= n) for row in range(n) ]
+Q_1 = [ And(Or( [Q[i][j] == 1 for j in range(n)] ) ) for i in range(n)]
 
-# At most one queen per column
-col_c = [ Distinct(Q) ]
+Q_2 = [ And( [ And( [ And( Or( Not(Q[i][j] == 1), Not(Q[i][k] == 1) ) ) for k in range(j+1, n) ] ) for j in range(n-1) ]) for i in range(n) ]
 
-# Diagonal constraint
-diag_c = [ If(i == j,
-              True,
-              And(Q[i] - Q[j] != i - j, Q[i] - Q[j] != j - i))
-           for i in range(n) for j in range(i) ]
+Q_3 = [ And( [ And( [ And( Or( Not(Q[i][j] == 1), Not(Q[k][j] == 1) ) ) for k in range(i+1, n) ] ) for i in range(n-1) ]) for j in range(n) ]
 
-solve(val_c + col_c + diag_c)
+Q_4 = [ And( [ And( [ And( Or( Not(Q[i][j] == 1), Not(Q[i-k][k+j] == 1) ) ) for k in range(min( i - 1, n - j)) ] ) for j in range(n-1) ]) for i in range(n) ]
+
+Q_5 = [ And( [ And( [ And( Or( Not(Q[i][j] == 1), Not(Q[i+k][k+j] == 1) ) ) for k in range(min( n - i, n - j)) ] ) for j in range(n-1) ]) for i in range(1, n-1) ]
+
+solve(Q_4 + Q_5)
+print()
+solve(Q_1 + Q_2 + Q_3)
+print()
+solve(Q_1 + Q_2 + Q_3 + Q_4 + Q_5)
+
