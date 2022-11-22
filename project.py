a24a475040dcfd9de734f793a3240a5a85774dab
alexstornant <stornant.alex@gmail.com>
2022-11-22 16:33:05 -0500

    refactored Q1-Q-3, still trying to get 4 and 5

--- project.py
+++ project.py
@@ -2,22 +2,27 @@ from z3 import *
 
 n = int(input("Please input a number: "))
 
-# Queen is in position Q[i][j], if Q[i][j] == 1, else 0
+# Queen is in position Q[i][j], if Q[i][j] == 1, else Q[i][j] != 0
 Q = [ [Int(f"Q_{i + 1}_{j + 1}") for j in range(n) ] for i in range(n) ]
 
-Q_1 = [ And(Or( [Q[i][j] == 1 for j in range(n)] ) ) for i in range(n)]
+Q_1 = And([ (Or( [Q[i][j] == 1 for j in range(n)] ) ) for i in range(n)])
 
-Q_2 = [ And( [ And( [ And( Or( Not(Q[i][j] == 1), Not(Q[i][k] == 1) ) ) for k in range(j+1, n) ] ) for j in range(n-1) ]) for i in range(n) ]
+Q_2 = And([Or( Not(Q[i][j] == 1), Not(Q[i][k] == 1) ) for i in range(n) for j in range(n-1) for k in range(j+1,n)])
 
-Q_3 = [ And( [ And( [ And( Or( Not(Q[i][j] == 1), Not(Q[k][j] == 1) ) ) for k in range(i+1, n) ] ) for i in range(n-1) ]) for j in range(n) ]
+Q_3 = And([Or( Not(Q[i][j] == 1), Not(Q[k][j] == 1) ) for j in range(n) for i in range(n-1) for k in range(i+1,n)])
 
-Q_4 = [ And( [ And( [ And( Or( Not(Q[i][j] == 1), Not(Q[i-k][k+j] == 1) ) ) for k in range(min( i - 1, n - j)) ] ) for j in range(n-1) ]) for i in range(n) ]
+Q_4 = And([Or(Not(Q[i][j] == 1), Not(Q[i-k][k+j] == 1)) for i in range(1, n) for j in range(n-1) for k in range(min(i-1, n-j))])
 
-Q_5 = [ And( [ And( [ And( Or( Not(Q[i][j] == 1), Not(Q[i+k][k+j] == 1) ) ) for k in range(min( n - i, n - j)) ] ) for j in range(n-1) ]) for i in range(1, n-1) ]
+Q_5 = And([Or(Not(Q[i][j] == 1), Not(Q[i+k][k+j] == 1)) for i in range(n-1) for j in range(n-1) for k in range(min(n-i, n-j))])
 
-solve(Q_4 + Q_5)
-print()
-solve(Q_1 + Q_2 + Q_3)
-print()
-solve(Q_1 + Q_2 + Q_3 + Q_4 + Q_5)
 
+s = Solver()
+s.add(Q_1, Q_2, Q_3)
+s.add(Q_4)
+s.add(Q_5)
+
+try:
+   s.check()
+   print(s.model())
+except:
+   print(s.check())
