42e11cd3847289cf1f5fcf9db94400853497f1a3
alexstornant <stornant.alex@gmail.com>
2022-11-22 19:58:32 -0500

    refactored Q_1 - Q_5 to finally work, also added pyplot to show what the board would look like

--- project.py
+++ project.py
@@ -1,31 +1,37 @@
 from z3 import *
+
+# Optional imports
 import numpy as np
 import matplotlib.pyplot as plt
+# End optional
 
 n = int(input("Please input a number: "))
 
 # Queen is in position Q[i][j], if Q[i][j] == 1, else Q[i][j] != 0
 Q = [ [Int(f"Q_{i + 1}_{j + 1}") for j in range(n) ] for i in range(n) ]
 
-Q_1 = And([ (Or( [Q[i][j] == 1 for j in range(n)] ) ) for i in range(n)])
+Q_1 = And([ Or( [Q[i][j] == 1 for j in range(n)] )  for i in range(n)])
 
-Q_2 = And([Or( Not(Q[i][j] == 1), Not(Q[i][k] == 1) ) for i in range(n) for j in range(n-1) for k in range(j+1,n)])
+Q_2 = And( [ And ( [ And( [Or( Not(Q[i][j] == 1), Not(Q[i][k] == 1)) for k in range(j+1,n)] )  for j in range(n-1)]) for i in range(n)] )
 
-Q_3 = And([Or( Not(Q[i][j] == 1), Not(Q[k][j] == 1) ) for j in range(n) for i in range(n-1) for k in range(i+1,n)])
+Q_3 = And( [ And ( [ And( [Or( Not(Q[i][j] == 1), Not(Q[k][j] == 1)) for k in range(i+1,n)] )  for i in range(n-1)]) for j in range(n)] )
 
-Q_4 = And([Or(Not(Q[i][j] == 1), Not(Q[i-k][k+j] == 1)) for i in range(1, n) for j in range(n-1) for k in range(min(i-1, n-j))])
+Q_4 = And( [ And ( [ And( [Or( Not(Q[i-1][j-1] == 1), Not(Q[i-k-1][k+j-1] == 1)) for k in range(1, min(i-1,n-j) + 1)] )  for j in range(1,n)]) for i in range(2,n+1)] )
+pp(Q_4)
 
-Q_5 = And([Or(Not(Q[i][j] == 1), Not(Q[i+k][k+j] == 1)) for i in range(n-1) for j in range(n-1) for k in range(min(n-i, n-j))])
+Q_5 = And( [ And ( [ And( [Or( Not(Q[i-1][j-1] == 1), Not(Q[i+k-1][k+j-1] == 1)) for k in range(1,min(n-i,n-j)+1)] )  for j in range(1,n)]) for i in range(1,n)] )
 
 s = Solver()
-s.add(Q_1, Q_2, Q_3)
-s.add(Q_4)
-s.add(Q_5)
+s.add(Q_1, Q_2, Q_3, Q_4, Q_5)
 
 try:
    s.check()
 
    m = s.model()
+   print(s.check())
+   print(m)
+
+   # Optional code to show board
    board = np.zeros((n,n))
 
    for i in range(n):
@@ -34,7 +40,12 @@ try:
             board[i][j] = 10
 
    plt.matshow(board)
+   plt.grid()
+   plt.xticks(np.arange(0.5,n,step=1))
+   plt.yticks(np.arange(0.5,n,step=1))
+   plt.set_cmap("Greens")
    plt.show()
+   # End optional
 
 except:
    print(s.check())
